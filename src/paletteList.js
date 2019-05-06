3aab6b75f1763f55d2856ca327aab4294ba450a7
PleatherShaman <shamanwebsolutions@gmail.com>
2019-05-06 16:48:08 +0800

    start styling PaletteList

--- src/paletteList.js
+++ src/paletteList.js
@@ -1,6 +1,42 @@
 import React, {Component } from 'react';
 import {Link} from 'react-router-dom'
 import MiniPalette from './miniPalette'
+import {withStyles} from "@material-ui/styles"
+
+
+
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    container:{
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection:"column",
+        flexWrap: "wrap",
+        border: "1px solid white"
+    },
+    nav: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        color: "white"
+
+    },
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3,30%)"
+    }
+
+}
+
 
 
 class PaletteList extends Component{
@@ -15,20 +51,33 @@ class PaletteList extends Component{
 
  
     render(){
-        const {palettes} = this.props
+        const {palettes, classes} = this.props
+        
 
 
         return(
 
-            <div>   
-                <MiniPalette/>
+            <div className={classes.root}>   
+                <div className={classes.container}>
+                    <div className={classes.nav}>
+                        <h1>ReactColors</h1>
+                    </div>
 
-                {palettes.map( palette => (
-                    <div>
-                        <MiniPalette {...palette}/>
-                        </div>
-                    
-                ))}
+                    <div className={classes.palettes}>
+
+                       {palettes.map( palette => (
+                         <div>
+                            <MiniPalette {...palette}/>
+                         </div>
+                        ))}
+                    </div>
+
+                </div>
+
+                
+                
+
+             
             </div>
             
             
@@ -38,4 +87,4 @@ class PaletteList extends Component{
 }
 
 
-export default PaletteList
+export default withStyles(styles)( PaletteList)
