9d80040ffbff2bcb1cac8eb7eb4f197a125f9005
PleatherShaman <shamanwebsolutions@gmail.com>
2019-05-07 15:18:08 +0800

    finish refactor of colorBox Css

--- src/colorBox.js
+++ src/colorBox.js
@@ -16,7 +16,8 @@ const styles = {
         cursor: "pointer",
         marginBottom: "-4.5px",
         "&:hover button": {
-            opacity: "1"
+            opacity: "1",
+            transition: "0.5s"
            
         }
     
@@ -64,8 +65,85 @@ const styles = {
         opacity: "0"
         
         
+    },
+
+    boxContent: {
+        position: "absolute",
+        width: "100%",
+        left: "0px",
+        bottom:"0px",
+        padding: "10px",
+        color: "black",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px"
+    },
+
+    copyOverlay: {
+        opacity: "0",
+        zIndex:"0",
+        width:"100%",
+        height:"100%",
+        transition: "transform 0.6s ease-in-out",
+        transform: "scale(0.1)"
+    
+    
+    },
+
+    copyMsg :{
+        position: "fixed",
+        left:"0",
+        right:"0",
+        top:"0",
+        bottom:"0",
+        display:"flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "4rem",
+        transform: "scale(0.1)",
+        color:"white",
+        opacity: "0",
+        zIndex:"25"
+    
+    },
+
+    showOverlay: {
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10",
+        position: "absolute"
+        
+    },
+
+    showMsg: {
+        opacity: "1",
+        transform: "scale(1)",
+        zIndex: "25",
+        transition: "all 0.4s ease-in-out",
+        transitionDelay: "0.3s",
+        "& h1": {
+            fontWeight: "400",
+            textShadow: "1px 2px black",
+            background: "rgba(255,255,255,0.2)",
+            width: "100%",
+            textAlign: "center",
+            textTransform: "uppercase",
+            marginBottom: "0",
+            padding: "1rem"
+            },
+
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: "100"
+        }
+            
+                            
+            
     }
+        
 }
+
 
 
 class ColorBox extends Component{
@@ -95,14 +173,14 @@ class ColorBox extends Component{
         return(
             <CopyToClipboard text = {background} onCopy={this.changeCopyState}>
                 <div className={classes.ColorBox} style={{background}}>
-                    <div className={`copy-overlay ${copied && "show"}`} style={{background}} />
-                    <div className={`copy-msg ${copied && "show"} ${classes.CopyText}`}>
+                    <div className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} style={{background}} />
+                    <div className={`${classes.copyMsg} ${copied && classes.showMsg} ${classes.CopyText}`}>
                         <h1 >Copied!</h1>
                         <p >{background}</p>
                     </div>
                     
-                    <div className="copy-container">
-                        <div className="box-content">
+                    <div >
+                        <div className={classes.boxContent}>
                             <span className={classes.colorName} >{name}</span>
                         </div>
                         <button className={classes.copyButton}>Copy</button>
