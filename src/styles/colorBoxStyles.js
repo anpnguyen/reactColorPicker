79ca809d50e0e4a3f8f28e52662107ad447a8f3f
PleatherShaman <shamanwebsolutions@gmail.com>
2019-05-09 17:28:40 +0800

    add responseiveness to paletteList

--- src/styles/colorBoxStyles.js
+++ src/styles/colorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js'
+import sizes from './sizes'
 
 export default {
 
@@ -14,6 +15,18 @@ export default {
             opacity: "1",
             transition: "0.5s"
            
+        },
+        [sizes.down('lg')]: {
+            width: "25%",
+            height: props => props.showFullPalette? "20%" :"50%"
+        },
+        [sizes.down('md')]: {
+            width: "50%",
+            height: props => props.showFullPalette? "10%" :"20%"
+        },
+        [sizes.down('xs')]: {
+            width: "100%",
+            height: props => props.showFullPalette? "5%" :"10%"
         }
     
     },
---
+++ src/styles/sizes.js
@@ -0,0 +1,25 @@
+
+
+export default {
+
+    up(){
+        const sizes = {
+            xs: "575.98px",
+            sm: "767.98px",
+            md: "991.98px",
+            lg: "1199.98px"
+        }
+
+    },
+
+    down(size){
+        const sizes = {
+            xs: "575.98px",
+            sm: "767.98px",
+            md: "991.98px",
+            lg: "1199.98px"
+        }
+
+        return `@media (max-width: ${sizes[size]})`
+    }
+}
